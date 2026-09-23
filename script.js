7c4f35af97dae33d263660599f45af392763fa08
gajadharkumar <kr2285763@gmail.com>
2026-09-24 00:39:48 +0530

    day1

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const h1 = document.querySelector('h1')
+const body=document.body;
+console.log(h1)
+h1.textContent='hey ami update ho chuka hu'
+
+h1.style.color="red"
+h1.style.fontSize="56px"
+h1.style.fontStyle="italic"
